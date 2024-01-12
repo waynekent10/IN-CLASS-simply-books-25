2ee3afc942616c60e8c69c21247987c4390b8702
Bruce Clark <Bruce.clark55@gmail.com>
2024-01-11 22:28:00 -0500

    added author form and getsingleauthor book data

---
+++ components/forms/AuthorForm.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { createAuthor, updateAuthor } from '../../api/authorData';
+import { useAuth } from '../../utils/context/authContext';
+
+const initialState = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  favorite: true,
+};
+
+function AuthorForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const [getAuthors, setAuthors] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getAuthors(user.uid).then(setAuthors);
+
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [getAuthors, obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateAuthor(formInput).then(() => router.push(`/author/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createAuthor(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateAuthor(patchPayload).then(() => {
+          router.push('/');
+        });
+      });
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Author</h2>
+
+      {/* FIRSTNAME INPUT  */}
+      <FloatingLabel controlId="floatingInput1" label="First Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter First Name"
+          name="first_name"
+          value={formInput.first_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* LASTNAME INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Last Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Last Name"
+          name="image"
+          value={formInput.last_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* Email INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="Email" className="mb-3">
+        <Form.Control
+          type="email"
+          placeholder="name@example.com"
+          name="email"
+          value={formInput.email}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* DESCRIPTION TEXTAREA  */}
+      <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
+        <Form.Control
+          as="textarea"
+          placeholder="Description"
+          style={{ height: '100px' }}
+          name="description"
+          value={formInput.description}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
+      <Form.Check
+        className="text-white mb-3"
+        type="switch"
+        id="favorite"
+        name="favorite"
+        label="Favorite?"
+        checked={formInput.favorite}
+        onChange={(e) => {
+          setFormInput((prevState) => ({
+            ...prevState,
+            sale: e.target.checked,
+          }));
+        }}
+      />
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Author</Button>
+    </Form>
+  );
+}
+
+AuthorForm.propTypes = {
+  obj: PropTypes.shape({
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    email: PropTypes.string,
+    favorite: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+    uid: '',
+  }),
+};
+
+AuthorForm.defaultProps = {
+  obj: initialState,
+};
+
+export default AuthorForm;
