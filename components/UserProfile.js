fb946e500e0b5e8050728a6929c0002a6a655372
Bruce Clark <Bruce.clark55@gmail.com>
2024-01-20 14:39:05 -0500

    userprofile error clear

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import { useAuth } from '../utils/context/authContext';
 
 export default function UserProfile() {
   const { user } = useAuth();
   return (
     <div>
-      <img src={user.photoURL} alt="userURL" width="100px" height="100px" />
+      <Image src={user.photoURL} alt="userURL" width="100px" height="100px" />
       <h1>{user.displayName}</h1>
       <h2>{user.email}</h2>
       <h3>{user.metadata.lastSignInTime}</h3>
